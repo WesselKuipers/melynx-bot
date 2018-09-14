1b32aaa01fad0377d6df1cf4839cd5019d6b2ab5
Wessel Kuipers <wesselkuipers@gmail.com>
2018-09-15 01:12:03 +0200

    added option to load token and prefix from ENV

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-//import  Settings from './interfaces/settings';
 import MelynxBot from './bot/bot';
 
-const options = require('./settings.json');
+let options;
+try {
+  options = require('./settings.json');
+} catch (e) { 
+  console.log('Could not find settings.json, loading from ENV instead.');
+  options = {
+    token: process.env.TOKEN,
+    prefix: process.env.PREFIX,
+  };
+}
+
 const bot = new MelynxBot(options);
 bot.run();
