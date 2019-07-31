a80f9156522d9ee624740d85b424925b31229832
Wessel Kuipers <wessel@d-centralize.nl>
2019-07-31 09:39:24 +0200

    removed unused typescript files

--- interfaces/settings.ts
+++
@@ -1,9 +0,0 @@
-import Discord from "discord.js";
-
-export interface Settings extends Discord.ClientOptions {
-    "token": string;
-    "prefix": string;
-    "disabledEvents": Discord.WSEventType[];
-}
-
-export default Settings;
