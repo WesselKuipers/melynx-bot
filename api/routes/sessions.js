1ceeb4b4ecec18d3ce44275631a30f428d5dccbb
Wessel Kuipers <wesselkuipers@gmail.com>
2020-01-04 21:58:24 +0100

    added filter for user guilds

--- api/routes/sessions.js
+++ api/routes/sessions.js
@@ -1,12 +1,22 @@
+import axios from 'axios';
+
 function getGuildSession(db) {
   return async (req, res) => {
     const { guildId } = req.params;
+
+    if (!req.headers.authorization) {
+      return res.status(401).send('Unauthorized');
+    }
+
+    const { data } = await axios.get('https://discordapp.com/api/users/@me/guilds', { headers: { 'Authorization': req.headers.authorization } });
+    const guilds = data.map(g => g.id);
+
     const sessions = guildId
       ? await db.models.session.findAll({
-          where: { guildId },
-          raw: true,
-        })
-      : await db.models.session.findAll({ raw: true });
+        where: { guildId },
+        raw: true,
+      })
+      : await db.models.session.findAll({ where: { guildId: { [db.Sequelize.Op.in]: guilds } }, raw: true });
     return res.send(sessions);
   };
 }
