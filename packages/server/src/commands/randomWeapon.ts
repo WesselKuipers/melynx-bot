bd0ea4ba3fb703aa2d956956aca84232c134d56e
Wessel Kuipers <wesselkuipers@gmail.com>
2022-01-04 00:17:42 +0100

    Remove commented code

--- packages/server/src/commands/randomWeapon.ts
+++ packages/server/src/commands/randomWeapon.ts
@@ -1,7 +1,4 @@
-// import { Message } from 'discord.js';
-// import { MelynxCommand, MelynxMessage } from '../types/melynxClient';
 import { join } from 'path';
-// import { MessageAttachment } from 'discord.js';
 
 const weapons = [
   'bow',
@@ -39,21 +36,3 @@ export const randomWeapon: MelynxCommand = {
     });
   },
 };
-
-// export default class RandomWeapon extends MelynxCommand {
-//   constructor() {
-//     super('randomWeapon', {
-//       aliases: ['randomWeapon', 'rw', 'weapon'],
-//       description: 'Returns a random weapon for you to use.`',
-//     });
-
-//     this.usage = '{prefix}randomWeapon';
-//   }
-
-//   public async exec(message: MelynxMessage): Promise<Message> {
-// const weapon = weapons[Math.floor(Math.random() * weapons.length)];
-// const attachment = new MessageAttachment(join(weaponPath, `${weapon}.png`));
-// attachment.height = 160;
-// return message.util.send(attachment);
-//   }
-// }
