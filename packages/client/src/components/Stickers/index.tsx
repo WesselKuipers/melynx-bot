6e1fdbf9d1528391f66f078c913b330b6d57a618
Wessel Kuipers <wesselkuipers@gmail.com>
2022-03-13 19:06:41 +0100

    Move all Component.ts to index.ts

--- packages/client/src/components/Stickers/index.tsx
+++ packages/client/src/components/Stickers/index.tsx
@@ -1,1 +1,67 @@
-export { default } from './Stickers';
+import Axios from 'axios';
+import { Card, Typography, Popover, Alert, Input } from 'antd';
+import styles from './index.module.css';
+import copy from 'copy-to-clipboard';
+import { useState, useCallback, ChangeEvent, useEffect } from 'react';
+
+const { Title } = Typography;
+const { Search } = Input;
+
+export default function Stickers() {
+  const [stickers, setStickers] = useState<string[]>([]);
+  const [activePopover, setActivePopover] = useState('');
+  const [filter, setFilter] = useState('');
+  let currentTimer: number;
+
+  const onSearchChange = useCallback((event: ChangeEvent<HTMLInputElement>): void => {
+    setFilter(event.target.value);
+  }, []);
+
+  useEffect(() => {
+    const getStickers = async () => {
+      const { data } = await Axios.get('/api/stickers');
+      setStickers(data);
+    };
+
+    getStickers();
+  }, []);
+
+  const copySticker = (sticker: string) => {
+    clearTimeout(currentTimer);
+    copy(`/sticker ${sticker.split('.')[0]}`);
+    setActivePopover(sticker);
+    currentTimer = window.setTimeout(() => setActivePopover(''), 2e3);
+  };
+
+  return (
+    <div className={styles.container}>
+      <Title level={3}>List of stickers</Title>
+      <Title level={4}>Click on one of the cards to copy the command!</Title>
+      <Search
+        style={{ width: 300 }}
+        onChange={onSearchChange}
+        placeholder="Search stickers"
+        allowClear
+      ></Search>
+      <div className={styles.stickers}>
+        {stickers
+          .filter((sticker) => !filter || sticker.includes(filter))
+          .map((sticker) => (
+            <Popover
+              key={sticker}
+              trigger="click"
+              placement="top"
+              content={<Alert message="Copied!" type="success" showIcon />}
+              visible={sticker === activePopover}
+              onVisibleChange={() => copySticker(sticker)}
+            >
+              <Card bodyStyle={{ paddingBottom: 10 }} className={styles.sticker} hoverable>
+                <img src={`${import.meta.env.API_URL}/assets/stickers/${sticker}`} />
+                <p>{sticker.split('.')[0]}</p>
+              </Card>
+            </Popover>
+          ))}
+      </div>
+    </div>
+  );
+}
