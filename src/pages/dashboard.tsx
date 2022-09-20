65c372569b705623fbee5d9ac4900934a16a1ce8
Wessel Kuipers <wesselkuipers@gmail.com>
2022-09-20 16:20:37 +0200

    Implement session management

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,12 +1,39 @@
-import { Container, Text } from '@mantine/core';
+import {
+  Button,
+  Center,
+  Container,
+  Loader,
+  Stack,
+} from '@mantine/core';
 import { GetServerSideProps } from 'next';
 import { unstable_getServerSession } from 'next-auth';
 import { authOptions } from './api/auth/[...nextauth]';
+import {
+  FaPlusCircle,
+} from 'react-icons/fa';
+import SessionCard from '../components/SessionCard';
+import { trpc } from '../utils/trpc';
 
 export default function Dashboard() {
+  const sessions = trpc.session.getSessions.useQuery();
+  if (!sessions.data) {
+    return (
+      <Center>
+        <Loader size="xl" />
+      </Center>
+    );
+  }
+
   return (
     <Container>
-      <Text>I’m the dashboard.</Text>
+      <Center mb="lg">
+        <Button leftIcon={<FaPlusCircle />}>Create new session</Button>
+      </Center>
+      <Stack>
+        {sessions.data.map((session) => (
+          <SessionCard session={session} key={session.id} onDelete={() => {}} onEdit={() => {}} />
+        ))}
+      </Stack>
     </Container>
   );
 }
--- src/server/trpc/routers/session.ts
+++ src/server/trpc/routers/session.ts
@@ -2,6 +2,9 @@ import axios from 'axios';
 import { protectedMiddleware } from '../middleware/protected';
 import { t } from '../trpc';
 import { Routes, RouteBases, RESTGetAPICurrentUserGuildsResult } from 'discord-api-types/v10';
+import { z } from 'zod';
+import { trpc } from '../../../utils/trpc';
+import { TRPCError } from '@trpc/server';
 
 export const sessionRouter = t.router({
   getSessions: t.procedure.use(protectedMiddleware).query(async ({ ctx }) => {
@@ -23,4 +26,68 @@ export const sessionRouter = t.router({
 
     return sessions;
   }),
+  updateSession: t.procedure
+    .use(protectedMiddleware)
+    .input(z.object({ id: z.string().regex(/d+{18}/), description: z.string().optional() }))
+    .mutation(async ({ ctx, input }) => {
+      const {prisma, session} = ctx;
+      const dbSession = await prisma.session.findFirst({
+        where: {
+          sessionId: input.id
+        },
+      });
+
+      if (!dbSession) {
+        throw new TRPCError({code: 'NOT_FOUND', message: 'Session not found.'})
+      }
+
+      if (dbSession.userId !== session.user.id) {
+        throw new TRPCError({
+          code: 'FORBIDDEN',
+          message: 'Only the session’s creator may edit sessions.'
+        });
+      }
+
+      const updatedSession = await prisma.session.update({
+        where: {
+          id: dbSession.id
+        },
+        data: {
+          description: input.description
+        }
+      });
+
+      return updatedSession;
+    }),
+  deleteSession: t.procedure
+    .use(protectedMiddleware)
+    .input(z.object({ id: z.string().regex(/d+{18}/) }))
+    .mutation(async ({ ctx, input }) => {
+      const { prisma, session } = ctx;
+      const dbSession = await prisma.session.findFirst({
+        where: {
+          sessionId: input.id,
+        },
+      });
+
+      if (!dbSession) {
+        throw new TRPCError({
+          code: 'NOT_FOUND',
+          message: 'Session not found.',
+        });
+      }
+
+      if (dbSession.userId !== session.user.id) {
+        throw new TRPCError({
+          code: 'FORBIDDEN',
+          message: 'Only the session’s creator may delete sessions.',
+        });
+      }
+
+      await prisma.session.delete({
+        where: {
+          id: dbSession.id,
+        },
+      });
+    }),
 });
